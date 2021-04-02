e8e54b49b4d5cfa14f8b6e2fbfbace68b85f5f1f
Matthew Jagiela <>
2021-04-02 18:34:45 -0400

    Initial Commit

---
+++ EarthQuake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EarthQuake
+//
+//  Created by Matthew Jagiela on 4/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
