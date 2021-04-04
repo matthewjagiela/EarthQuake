bafea2ac4568fb33d7d8af0848703a3e00c5e12e
Matthew Jagiela <>
2021-04-03 23:51:15 -0400

    chore: API unit testing

---
+++ EarthQuakeTests/APITest.swift
@@ -0,0 +1,29 @@
+//
+//  APITest.swift
+//  EarthQuakeTests
+//
+//  Created by Matthew Jagiela on 4/3/21.
+//
+
+import XCTest
+@testable import EarthQuake
+class APITest: XCTestCase {
+    let api = APIHandler()
+    func testAPIWorking() {
+        let apiExpectation = expectation(description: "APIResponse")
+        api.fetchEarthQuakeData { (response, data) in
+            switch response {
+            case .success:
+                XCTAssertNotNil(data)
+            case .APIError(let code):
+                XCTFail("API FAILURE = \(code)")
+            default:
+                XCTFail("Error on our end")
+            }
+            apiExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
+    }
+    
+}
